cf06f4cc0b26460b6702bf790f17acb94a5fe551
Айгул Суйунева <ajgulsujunova@gmail.com>
2022-11-22 17:05:55 +0300

    js1

---
+++ script/script.js
@@ -0,0 +1,42 @@
+const num =1992
+console.log( num )
+
+const a =10
+const b =2
+const c =5
+console.log( a + b + c )
+
+const hundred = 100
+console.log( hundred * 2)
+
+const ss =67
+console.log( ss % 7)
+
+const a1 =10
+const b1 =2
+console.log(a1 + b1)
+console.log(a1 - b1)
+console.log(a1 * b1)
+console.log(a1 / b1)
+
+const c2 =15
+const d =2
+console.log( c2 + d)
+const result=12
+console.log( result)
+
+const a2 =30
+const b2 =15
+console.log(a2 + b2)
+const c3 =45
+console.log(c3 % 45)
+
+
+const a3 = 17
+const b3 = 10
+console.log( a3 -b3)
+const c4 = 7
+const d3 =100
+console.log( c4 +d3 )
+const result1=107
+console.log( result1 )
